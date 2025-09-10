ac132f8aedf2776193ce897b9a9022e2dd7329db
otmanix <elaloi.otmane@gmail.com>
2025-09-10 20:53:27 +0200

    fix: quick fixes

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const base = process.env.BASE_PATH || '/'
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 })
