099f1ffe799fc795983585163e6a9957464424b1
otmanix <elaloi.otmane@gmail.com>
2025-11-28 22:38:02 +0100

    revert vite base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-const base = process.env.BASE_PATH || '/'
+//const base = process.env.BASE_PATH || '/'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: base,
+  base: '/',
 })
